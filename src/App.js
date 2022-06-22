5fd14ad1d33b049a8ad79bce4c455b040a967b3d
ram kumar <rammaniyari@gmail.com>
2022-06-22 12:54:12 +0530

    second

--- src/App.js
+++ src/App.js
@@ -8,22 +8,16 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <>
-      
+    <> 
       <BrowserRouter>
-      <Navbar />
+    
         <Routes>
-        {/* <Route path='/' element = { <h1>Home component</h1> } />
-        <Route path='/about' element = { <h1>About component</h1> } />
-        <Route path='/details' element = { <h1>details component</h1> } /> */}
-
-        {/* <Route path='/' element =  {<Home />} />
-        <Route path='/about' element =  {<About />} />
-        <Route path='/details' element =  {<Details />} /> */}
-        
-
-        
-        <Route path='/' element =  {<Home />} />
+        <Route path='/' element =  {
+        <div>
+          <Navbar />
+        <Home />
+        </div>
+        } />
         <Route path='/about' element =  {<About />} />
         <Route path='/details' element =  {<Details />} />
         </Routes>
